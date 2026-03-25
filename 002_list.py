3cf5301f2621bc8cf32fa4fa3c339af7f2cf5f43
LAPTOP-JREMQ6PU\HP <s06.patil@gmail.com>
2026-03-25 15:18:04 +0530

    added list

---
+++ 002_list.py
@@ -0,0 +1,29 @@
+# LIST
+# [], index, duplicates allowed, ordered, mutable
+
+my_list = [1, 3, 2, 5, 5]
+
+# access first element
+print(my_list[0])
+
+# access second element
+print(my_list[1])
+
+# access first three element
+print(my_list[0:3])
+
+# access first and third element 
+print(my_list[0:3:2])
+
+# last element
+print(my_list[-1])
+
+# last element
+print(my_list[-2])
+
+# check for duplciates
+print(my_list)
+
+# replace first item with  1000
+my_list[0] = 1000
+print(my_list)
